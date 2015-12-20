552b79b2ece8354c6bfbc80e540f91becb0bc345
Tilo <tilo.kontakt@gmail.com>
2015-12-20 16:54:55 +0100

    Fehlende Dateien hinzugefügt.

---
+++ Retro_Indie_Spiel_Webserver/Retro_Indie_Spiel_Webserver/Controllers/HighscoresController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Retro_Indie_Spiel_Webserver.Models;
+using System.Security.Cryptography;
+
+namespace Retro_Indie_Spiel_Webserver.Controllers
+{
+    public class HighscoresController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Highscores
+        public ActionResult List()
+        {
+            var highscores = db.Highscores.Include(e => e.User).OrderByDescending(e => e.Score).ToList();
+            List<HighscoreListViewModel> list = new List<HighscoreListViewModel>();
+            int counter = 1;
+            foreach(HighscoreModel high in highscores)
+            {
+                list.Add(HighscoreListViewModel.build(high, counter));
+                counter++;
+            }
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index([Bind(Include = "Name,Score,md5Hash")] HighscoreViewModel highscoreViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string source = highscoreViewModel.Name + highscoreViewModel.Score + ApplicationDbContext.md5HashKey;
+                string hash = ApplicationDbContext.GetMd5Hash(MD5.Create(), source);
+                if (hash != highscoreViewModel.md5Hash)
+                {
+                    return new HttpUnauthorizedResult();
+                }
+                ApplicationUser User = db.Users.Where(e => e.UserName == highscoreViewModel.Name).First();
+                if( User == null)
+                {
+                    return HttpNotFound();
+                }
+                HighscoreModel highscoreModel = new HighscoreModel
+                {
+                    UserId = User.Id,
+                    Score = highscoreViewModel.Score
+                };
+                if (db.Highscores.Where(e => e.Score == highscoreModel.Score && e.UserId == highscoreModel.UserId).Count() == 0)
+                {
+                    db.Highscores.Add(highscoreModel);
+                    db.SaveChanges();
+                    return Content("Ok First");
+                }
+
+                return Content("Ok Already");
+            }
+            string errorList = "";
+            foreach (ModelState modelState in ViewData.ModelState.Values)
+            {
+                foreach (ModelError error in modelState.Errors)
+                {
+                    errorList += " // " + error.ErrorMessage;
+                }
+            }
+            return Content("Error: " + errorList);
+        }
+
+        // GET: Highscores/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HighscoreModel highscoreModel = db.Highscores.Find(id);
+            if (highscoreModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscoreModel);
+        }
+
+        // POST: Highscores/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,Score")] HighscoreModel highscoreModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(highscoreModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(highscoreModel);
+        }
+
+        // GET: Highscores/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HighscoreModel highscoreModel = db.Highscores.Find(id);
+            if (highscoreModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscoreModel);
+        }
+
+        // POST: Highscores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            HighscoreModel highscoreModel = db.Highscores.Find(id);
+            db.Highscores.Remove(highscoreModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Retro_Indie_Spiel_Webserver/Retro_Indie_Spiel_Webserver/Models/HighscoreModels.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Retro_Indie_Spiel_Webserver.Models
+{
+    [Table("Highscore")]
+    public class HighscoreModel
+    {
+        
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string UserId { get; set; }
+        
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Score { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+    }
+
+    public class HighscoreViewModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Score { get; set; }
+
+        [Required]
+        public string md5Hash { get; set; }
+    }
+
+    public class HighscoreListViewModel
+    {
+        public static HighscoreListViewModel build(HighscoreModel model, int position)
+        {
+            return new HighscoreListViewModel { Name = model.User.UserName, Score = model.Score, Position = position };
+        }
+        
+        public int Position { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
