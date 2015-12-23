1d49e0c0d3676414fac2de9ab2765ad6a6da7402
Tilo <tilo.kontakt@gmail.com>
2015-12-23 10:25:34 +0100

    Favicon ergänzt.

--- Retro_Indie_Spiel_Webserver/Retro_Indie_Spiel_Webserver.Tests/Controllers/AcccountControllerTest.cs
+++ Retro_Indie_Spiel_Webserver/Retro_Indie_Spiel_Webserver.Tests/Controllers/AcccountControllerTest.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Retro_Indie_Spiel_Webserver.Controllers;
 namespace Retro_Indie_Spiel_Webserver.Tests.Controllers
 {
     [TestClass]
     public class AcccountControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegisterLogin()
         {
+            AccountController account = new AccountController();
+
+            account.Index();
+
         }
     }
 }
